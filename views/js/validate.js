3da63bbe9bca72da3c722dca50723d3a2834fd1d
ngthuongdoan <ngthuongdoan@gmail.com>
2020-02-26 09:57:06 +0700

    Retype password done; Close #11

--- views/js/validate.js
+++ views/js/validate.js
@@ -1,6 +1,8 @@
 const submitForm = () => {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
+  let confirm = document.getElementById("confirm").value;
+
   let error = false;
   init();
   if (username.includes(" ")) {
@@ -9,16 +11,20 @@ const submitForm = () => {
     ).textContent = `Username mustn't have space`;
     error = true;
   }
-  if (password.length < 6 || password.includes(' ')) {
+  if (password.length < 6 || password.includes(" ")) {
     document.getElementById(
       "passwordError"
     ).textContent = `Password must have at least 6 characters and not contains space`;
     error = true;
+  } else {
+    if (confirm !== password) {
+      document.getElementById(
+        "passwordError"
+      ).textContent = `Please confirm your password again`;
+      error = true;
+    }
   }
-  if (error) {
-    return false;
-  } 
-  return true;
+  return error ? false : true;
 };
 
 const init = () => {
