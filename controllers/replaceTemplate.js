5ff24d0e17d6d174d7683fd34c4072a5f6a12570
ngthuongdoan <ngthuongdoan@gmail.com>
2020-02-27 21:05:06 +0700

    Add replace Account info

--- controllers/replaceTemplate.js
+++ controllers/replaceTemplate.js
@@ -1,6 +1,7 @@
 exports.replacePopupTemplate = (state, position, content, template) => {
-  if(content === "") return template.toString().replace(position, "");
-  let co = (!state)? `<script>
+  if (content === "") return template.toString().replace(position, "");
+  let co = !state
+    ? `<script>
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
@@ -18,4 +19,15 @@ exports.replacePopupTemplate = (state, position, content, template) => {
 
 exports.replaceTemplate = (position, content, template) => {
   return template.toString().replace(position, content);
+};
+
+exports.replaceAccountTemplate = (session, template) => {
+  let result = this.replaceTemplate(
+    "{% ACCOUNTNAME %}",
+    session.fullname,
+    template
+  );
+  result = this.replaceTemplate("{% AVATAR %}", session.avatar, result);
+  result = this.replaceTemplate("{% USERNAME %}", session.username, result);
+  return result.toString();
 };
