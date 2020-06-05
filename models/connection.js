5cb87e75f79211684ad877e1a3d0fecd447302d7
ngthuongdoan <ngthuongdoan@gmail.com>
2020-06-05 12:42:41 +0700

    change database

--- models/connection.js
+++ models/connection.js
@@ -1,20 +1,14 @@
 const mysql = require("mysql");
 
-const databaseName =
-  process.env.ENVIRONMENT == "dev"
-    ? process.env.MYSQL_DATABASE
-    : process.env.MYSQL_REAL_DATABASE;
-
 const conn = mysql.createConnection({
   host: process.env.MYSQL_URL,
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
-  database: databaseName,
+  database: process.env.MYSQL_DATABASE,
 });
 
 conn.connect((err) => {
   err ? console.log("Turn on MySQL service") : console.log("Connected");
-  console.log(databaseName);
 });
 
 module.exports = conn;
